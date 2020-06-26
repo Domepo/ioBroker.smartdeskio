de8da4ccf1080a3dcde564612d0a566ff2d880fe
unknown <dominiktb2@gmail.com>
2020-06-26 14:06:39 +0200

    test

--- main.js
+++ main.js
@@ -43,16 +43,12 @@ class Smartdeskio extends utils.Adapter {
 		Here a simple template for a boolean variable named "testVariable"
 		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
 		*/
-		var zahl; 
 
-		let test = function(){    
-			zahl = zahl + 1;
-			return zahl;
-		}
-		await this.setObjectNotExistsAsync("testVariable", {
+		await this.setObjectNotExistsAsync("testVariable", {	
+
 			type: "state",
 			common: {
-				name: setInterval(function(){return test(); }, 100);,
+				name: "testVariabletestVariable",
 				type: "boolean",
 				role: "indicator",
 				read: true,
@@ -73,7 +69,7 @@ class Smartdeskio extends utils.Adapter {
 			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
 		*/
 		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
+		await this.setStateAsync("testVariable", 123);
 
 		// same thing, but the value is flagged "ack"
 		// ack should be always set to true if the value is received from or acknowledged from the target system
